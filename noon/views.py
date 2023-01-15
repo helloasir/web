41fe1766d43d0e85e04ccea38e25225c60c285e5
Hello Asir <helloasir@gmail.com>
2023-01-15 15:52:10 +0400

    static added

--- noon/views.py
+++ noon/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
+time = datetime.datetime.now()
 
 def index(request):
-    return HttpResponse("Hello, you are in noon click <a href=/>Home </a> or <a href=/weblist>weblist </a> or <a href=/noon >Noon </a> or <a href= page1 >Page1 </a> ")
+    return HttpResponse("Hello, Time now "+str(time)+" you are in noon click <a href=/>Home </a> or <a href=/weblist>weblist </a> or <a href=/noon >Noon </a> or <a href= page1 >Page1 </a> ")
 
 def page1(request):
     return HttpResponse("Hello, you are in Page 1 <a href=/>Home </a> or <a href=/weblist>weblist </a> or <a href=/noon >Noon </a> ")
